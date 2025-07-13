6533c35edec4443bc4c354bb20f61178d92b6a45
vearne <asdwoshiaotian@gmail.com>
2025-07-13 10:40:49 +0800

    update src/vearne_akshare_mcp/__main__.py

--- src/vearne_akshare_mcp/__main__.py
+++ src/vearne_akshare_mcp/__main__.py
@@ -22,7 +22,7 @@ def main():
 
     mcp = create_mcp()
     if args.http:
-        mcp.run(transport="streamable-http", port=8902)
+        mcp.run(transport="streamable-http", bind=args.bind, port=args.port)
     else:
         mcp.run()
 
